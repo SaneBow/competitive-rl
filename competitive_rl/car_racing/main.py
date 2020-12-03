1bbe265aebd14e6760eb7f5e3a9fbe8653bb5036
X.B. Wang <sanebow@gmail.com>
2020-12-03 11:31:39 +0800

    test no-brake bug

--- competitive_rl/car_racing/main.py
+++ competitive_rl/car_racing/main.py
@@ -1,12 +1,21 @@
-from competitive_rl.car_racing.car_racing_multi_players import CarRacing
+from car_racing_multi_players import CarRacing
 
 if __name__ == "__main__":
     env = CarRacing(num_player=1)
     # example: env.reset(use_local_track="./track/test.json",record_track_to="")
     # example: env.reset(use_local_track="",record_track_to="./track")
     env.reset(use_local_track="", record_track_to="")
+    cnt = 0
     while True:
         env.render()
-        observation, reward, done, info = env.step(env.action_space.sample())
+        # observation, reward, done, info = env.step(env.action_space.sample())
+        if cnt < 10:
+            observation, reward, done, info = env.step([0, 1])
+        if cnt > 10:
+            observation, reward, done, info = env.step([0, -1])
+            # observation, reward, done, info = env.step([0, 0])  # with the no-brake bug, car's behavior is the same as always setting brake to 0
+        cnt += 1
+        if cnt == 20:
+            cnt = 0
         if env.show_all_car_obs:
             env.show_all_obs(observation)
